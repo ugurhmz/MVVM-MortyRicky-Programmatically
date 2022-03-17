2add06298a09d2c3ec4c89bce30cd403ff17ad9e
ugurhmz <craxx67@gmail.com>
2022-03-17 17:47:44 +0300

    first

---
+++ RickyMorty-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyMorty-MVVM
+//
+//  Created by ugur-pc on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
